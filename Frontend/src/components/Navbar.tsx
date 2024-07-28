5b1ac8c1c9b12ecc40d3bfb62ef34008c81aa97f
Anand Prabhu <anandprabhu530@gmail.com>
2024-07-28 17:09:31 +0530

    Button changes

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <div className="flex border-2 border-red-400 w-full p-4 px-8 justify-between items-center">
       <div className="text-2xl font-semibold">Story Pulse</div>
-      <Button>Export as PDF</Button>
+      <Button className="bg-slate-900 text-white hover:bg-neutral-700  ">
+        Export as PDF
+      </Button>
     </div>
   );
 };
