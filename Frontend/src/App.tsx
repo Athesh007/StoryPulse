556265b81af457fc07d5a22452c36baa96a970de
Anand Prabhu <anandprabhu530@gmail.com>
2024-07-29 16:55:36 +0530

    State for local setup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -27,6 +27,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Textarea } from "./components/ui/textarea";
+import { useState } from "react";
 
 const frameworks = [
   {
@@ -84,121 +85,142 @@ const FormSchema = z.object({
 });
 
 const App = () => {
+  const [data, setData] = useState<{ test1: string; test2: string }[]>([]);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
+    const a = { test1: "test1", test2: "test2" };
+    const b = { test1: "test1", test2: "test2" };
+    setData((prev) => [...prev, a, b]);
+  }
+  {
+    console.log(`Data:${data}\n Length:${data.length}`);
   }
   return (
     <div className="w-full font-sans border-2 border-black min-h-screen flex flex-col items-center">
       <Navbar />
       <div className="rounded-lg w-[40rem]">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col rounded-lg border border-neutral-300 shadow-xl p-4 "
-          >
-            <FormField
-              control={form.control}
-              name="userdata"
-              render={() => (
-                <FormItem>
-                  <div className=" w-full ">
-                    <Textarea
-                      className="py-2 pr-2 bg-transparent w-full outline-none resize-none border-none placeholder:text-neutral-400 font-medium"
-                      placeholder="Ask anything..."
-                      onChange={(event) => {
-                        form.setValue("userdata", event.target.value);
-                      }}
-                    />
-                  </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex justify-between items-center pt-4">
-              <FormField
-                control={form.control}
-                name="genre"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col border border-neutral-300 rounded-lg">
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant="secondary"
-                            role="combobox"
-                            className={cn(
-                              "w-[200px] justify-between",
-                              !field.value && "text-muted-foreground"
-                            )}
-                          >
-                            {field.value
-                              ? frameworks.find(
-                                  (framework) => framework.value === field.value
-                                )?.label
-                              : "Select genre"}
-                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-[200px] mt-2 p-0 bg-white">
-                        <Command>
-                          <CommandInput placeholder="Search genre..." />
-                          <CommandList>
-                            <CommandEmpty>No genre found.</CommandEmpty>
-                            <CommandGroup>
-                              {frameworks.map((framework) => (
-                                <CommandItem
-                                  value={framework.label}
-                                  key={framework.value}
-                                  onSelect={() => {
-                                    form.setValue("genre", framework.value);
-                                  }}
-                                >
-                                  <Check
-                                    className={cn(
-                                      "mr-2 h-4 w-4",
-                                      framework.value === field.value
-                                        ? "opacity-100"
-                                        : "opacity-0"
-                                    )}
-                                  />
-                                  {framework.label}
-                                </CommandItem>
-                              ))}
-                            </CommandGroup>
-                          </CommandList>
-                        </Command>
-                      </PopoverContent>
-                    </Popover>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" className="px-0">
-                <div className="bg-neutral-200 p-2 rounded-lg cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-6 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                    />
-                  </svg>
+        {data.length === 0 ? (
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col rounded-lg border border-neutral-300 shadow-xl p-4 "
+              >
+                <FormField
+                  control={form.control}
+                  name="userdata"
+                  render={() => (
+                    <FormItem>
+                      <div className=" w-full ">
+                        <Textarea
+                          className="py-2 pr-2 bg-transparent w-full outline-none resize-none border-none placeholder:text-neutral-400 font-medium"
+                          placeholder="Ask anything..."
+                          onChange={(event) => {
+                            form.setValue("userdata", event.target.value);
+                          }}
+                        />
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex justify-between items-center pt-4">
+                  <FormField
+                    control={form.control}
+                    name="genre"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col border border-neutral-300 rounded-lg">
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant="secondary"
+                                role="combobox"
+                                className={cn(
+                                  "w-[200px] justify-between",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value
+                                  ? frameworks.find(
+                                      (framework) =>
+                                        framework.value === field.value
+                                    )?.label
+                                  : "Select genre"}
+                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-[200px] mt-2 p-0 bg-white">
+                            <Command>
+                              <CommandInput placeholder="Search genre..." />
+                              <CommandList>
+                                <CommandEmpty>No genre found.</CommandEmpty>
+                                <CommandGroup>
+                                  {frameworks.map((framework) => (
+                                    <CommandItem
+                                      value={framework.label}
+                                      key={framework.value}
+                                      onSelect={() => {
+                                        form.setValue("genre", framework.value);
+                                      }}
+                                    >
+                                      <Check
+                                        className={cn(
+                                          "mr-2 h-4 w-4",
+                                          framework.value === field.value
+                                            ? "opacity-100"
+                                            : "opacity-0"
+                                        )}
+                                      />
+                                      {framework.label}
+                                    </CommandItem>
+                                  ))}
+                                </CommandGroup>
+                              </CommandList>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="px-0">
+                    <div className="bg-neutral-200 p-2 rounded-lg cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6 "
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                        />
+                      </svg>
+                    </div>
+                  </Button>
                 </div>
-              </Button>
-            </div>
-          </form>
-        </Form>
+              </form>
+            </Form>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-6">
+            {data.map((individual_data, index: number) => (
+              <div className="text-xl font-semibold flex gap-10" key={index}>
+                <div> {individual_data.test1}</div>
+                <div> {individual_data.test2}</div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
