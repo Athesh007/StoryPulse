53e92be9b09a49f9aad846407611ea5f81ccbdb2
Anand Prabhu <anandprabhu530@gmail.com>
2024-08-02 22:07:07 +0530

    Trying to figureout context awarness

--- Backend/index.js
+++ Backend/index.js
@@ -14,15 +14,20 @@ app.get("/", (req, res) => {
 });
 
 // Define your model
+const model = new ChatGoogleGenerativeAI.ChatGoogleGenerativeAI({
+  model: "gemini-1.5-flash",
+  apikey: process.env.GOOGLE_API_KEY,
+});
 
 //test api
 app.post("/generate", async (req, res) => {
   console.log("entered");
-  const model = new ChatGoogleGenerativeAI.ChatGoogleGenerativeAI({
-    model: "gemini-1.5-flash",
-    apikey: process.env.GOOGLE_API_KEY,
-  });
   const browserSchema = z.object({
+    strory: z
+      .string()
+      .describe(
+        "Generate a story with the initital request or follow based on the story that user choose."
+      ),
     story1: z
       .string()
       .describe("Follow or create a story1 based on the same given request"),
@@ -36,14 +41,22 @@ app.post("/generate", async (req, res) => {
   const llmWithStructuredOutput = model.withStructuredOutput(browserSchema, {
     name: "story",
   });
+
   const structuredOutputRes = await llmWithStructuredOutput.invoke([
     [
-      "human",
+      "system",
       "You will be provided with some input for the first time with a requirement and then you need to create the story or follow with the choices made. Generate a horror, supernatural story based on a man in the middle of a deep jungle.",
     ],
+
+    ["human", "generate a story or if choice available follow up on that"],
   ]);
 
   console.log(structuredOutputRes);
+
+  const followup = await llmWithStructuredOutput.invoke("I choose story 2");
+
+  console.log(followup);
+
   const data = {
     story1:
       "A man is alone in the heart of an impenetrable jungle. The canopy is so thick that even the moon's light struggles to pierce through, casting eerie shadows that dance and distort in the undergrowth. His supplies are dwindling, and a growing sense of paranoia gnaws at him. Strange noises echo through the dense foliage, and as night descends, a blood-curdling scream shatters the silence. Fear, hunger, and the unknown conspire to push him to the brink of sanity.",
